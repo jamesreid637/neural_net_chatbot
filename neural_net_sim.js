33dc8942d357fb31ab800fe0bc99703cf4cc1038
James Reid <jamesreid637@gmail.com>
2017-02-15 18:27:19 +0000

    implementing random-word library

--- neural_net_sim.js
+++ neural_net_sim.js
@@ -1,1 +1,10 @@
-// test commit
+const randomWord = require("random-word");
+var words = [];
+function addDict(dict, key, value) {
+    dict.push({ key: key, value: value });
+};
+for (i = 0; i < 15; i++){
+    word = randomWord();
+    addDict(words, i, word);
+};
+console.log(words)
